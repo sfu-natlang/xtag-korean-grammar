1deb2dce2c65f821ab2d575a62d0190bbbcf89da
Anoop Sarkar <anoop@sfu.ca>
2013-11-07 11:20:14 -0800

    files imported from seas.upenn.edu

---
+++ examples.ex
@@ -0,0 +1,24 @@
+thamci hwaltongun motun cihwi pwunyaeyse palsaynghanta .
+yenlyeycekulo kemthohanta .
+pon tankyeynun twu pwupwunulo nanwuecinta .
+cek swucip wihyep databaselul yucihanta .
+cakcenpoanuy kwacengun tophyo9-1ey nathana issta .
+siceng hwaltongun phichukceng pwutayuy kwenhanita .
+i kwacengun tankyeycekulo palsayngtoynun kesun anita .
+kak ceytayey haytanghanun cakcenpoansilmwutanul kwusenghanta .
+cihwikwaneykey cekcelhan cochiuy sihayngul kenuyhayeya hanta .
+pwuchem nanun pwulok talul wihan yeymwunul ceykonghanta .
+kakkakuy  akwuncheppokiponyosonun pimwun ttonun phyengmwunil kesita .
+cemcungcekulo akwuncheppokiponyosonun pimwuni toyl kesita .
+cakcenpoanun cihwithongceycen cenlyakey issese hayksim pwupwunita .
+silceycekulo  kakki talun tankyeyka tongsitapallo palsaynghanta .
+cheppo kiponyosolul palcensikhinta .
+inun pimwun hokun phyengmwunulo toye issta  .
+poankwa kimanun sanghopowan kwankyeyita .
+poanun cihwithongceycen kwacenguy thonghaptoyn pwupwunita .
+inun cakcenuy wancenhan ihayeyto towumi toynta .
+pimilcheppoeyse cacwu palsaynghanun chwiyaksengun wuyenhan kyengwuey
+ilenanta .
+yenhapsanun C2-kongkyek hwaltongey tayhan chaykimi issta .
+salyengkwanun  sahanguy sihayngul pocanghayya hanta .
+salyengkwanul pohohal philyonun epsta .
